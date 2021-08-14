f0392c8d8ccd2fdf852e450b2ae3c0415504417b
mgs28-mh <gsaputra880@gmail.com>
2021-08-14 18:58:05 +0700

    dirac: s/VENDOR_PATH/DIRAC_PATH
    Signed-off-by: mgs28-mh <gsaputra880@gmail.com>

--- dirac.mk
+++ dirac.mk
@@ -1,19 +1,19 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/dirac
 
-VENDOR_PATH := vendor/xiaomi/dirac
+DIRAC_PATH := vendor/xiaomi/dirac
 
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
-    $(VENDOR_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
-    $(VENDOR_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
-    $(VENDOR_PATH)/vendor/etc/diracvdd.bin:$(TARGET_COPY_OUT_VENDOR)/etc/diracvdd.bin
+    $(DIRAC_PATH)/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    $(DIRAC_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
+    $(DIRAC_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    $(DIRAC_PATH)/vendor/etc/diracvdd.bin:$(TARGET_COPY_OUT_VENDOR)/etc/diracvdd.bin
 
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DIRAC_PATH)/sepolicy
 
 # Dirac
 PRODUCT_PACKAGES += \
     XiaomiDirac \
 
 # Props ($PATH_PROP)
-TARGET_PRODUCT_PROP += $(VENDOR_PATH)/dirac.prop
+TARGET_PRODUCT_PROP += $(DIRAC_PATH)/dirac.prop
