4c5af05e660944bc8bd19d1ad7cc201caf836d4d
Genkzsz11 <Genkzsz11@gmail.com>
2021-09-17 17:31:30 +0700

    Dirac >> XiaomiParts
    Features included
     * Dirac
     * FPS Info
     * KCal
     * Ambient Display Gestures
     * 3-Way System Vibration Control
     * Restore Setting on Boot
     * USB Fastcharge
     * Sound Control
    
     * Rootdir also included as well
    
    Features Removed
     * Touchboost
     * SELinux Switch
     * Logtile
     * Torch Settings
    
    Co-authored-by: reharsh67 <reharshdeshpande@gmail.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: Takeshiro04 <gianashtonreyes@gmail.com>
    Co-authored-by: smokey18 <kmboplays@gmail.com>
    Signed-off-by: smokey18 <kmboplays@gmail.com>

---
+++ part.mk
@@ -0,0 +1,20 @@
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/part
+
+PART_PATH := vendor/xiaomi/part
+
+PRODUCT_COPY_FILES += \
+    $(PART_PATH)/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    $(PART_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
+    $(PART_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    $(PART_PATH)/vendor/etc/diracvdd.bin:$(TARGET_COPY_OUT_VENDOR)/etc/diracvdd.bin
+
+BOARD_SEPOLICY_DIRS += $(PART_PATH)/sepolicy
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts \
+    init.xiaomi_parts.rc
+
+# Props ($PATH_PROP)
+TARGET_PRODUCT_PROP += $(PART_PATH)/dirac.prop
